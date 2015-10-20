4b20184a8ea69751082421b2127473c330ef6b8a
LawlietBlack <kenneth.black@live.com>
2015-10-20 06:16:42 -0700

    fixed axes, added hover effect

--- client/app/main/main.controller.js
+++ client/app/main/main.controller.js
@@ -90,53 +90,90 @@ angular.module('stocksApp')
     //set up x variables
     //set up xDomain by parsing time and passing it into d3.extent
     var parseTime = d3.time.format('%Y-%m-%dT%H:%M:%S');
-    var xScale = d3.time.scale().range([600, 0]);
+    var xScale = d3.time.scale().range([900, 0]);
     var xAxis = d3.svg.axis().scale(xScale).orient('bottom').ticks(10);
+    var xMin = d3.min(data.Dates, function(d) {
+      var time = parseTime.parse(d);
+      time.setMonth(time.getMonth() - 1);
+      return time
+    });
+    var xMax = d3.max(data.Dates, function(d) {
+      var time = parseTime.parse(d);
+      time.setMonth(time.getMonth() + 1);
+      return time
+    });
+    console.log([xMin, xMax])
+    xScale.domain([xMin, xMax]);
     $scope.viz.append('g')
       .attr('class', 'x axis')
       .call(xAxis)
-    var xDomain = d3.extent(data.Dates, function(d) {
-      return parseTime.parse(d)
-    });
-    xScale.domain(xDomain);
+      .selectAll('text')
+      .attr('transform', function() {
+        return 'rotate(-65)'
+      })
+      .style('text-anchor', 'end')
+      .style('font-size', '10px')
+      .attr('cx', '-10px')
+      .attr('cy', '10px')
 
 
     //set up y variables
     //Set up yDomain by combining all datasets to one array, then using d3.extent
     var yScale = d3.scale.linear().range([400,0]);
     var yAxis = d3.svg.axis().scale(yScale).orient('left').ticks(10);
-    $scope.viz.append('g')
-      .attr('class', 'y axis')
-      .call(yAxis);
     var allData = [];
     for(var i=0;i<data.Elements.length;i++) {
       allData = allData.concat(data.Elements[i].DataSeries.close.values)
     }
     var yDomain = d3.extent(allData, function(d) {
-      return parseInt(d)
+      return parseInt(d)*1.1;
     });
     yScale.domain(yDomain);
-
+    $scope.viz.append('g')
+      .attr('class', 'y axis')
+      .call(yAxis);
 
 
     for(var i=0;i< data.Elements.length; i++) {
 
-      var dots = $scope.viz.append('g')
-        .attr('class', data.Elements[i].Symbol)
-        .attr('class', 'data-set')
-        .selectAll('circle')
+      var dots = $scope.viz.selectAll('g.dots.data-set.' + data.Elements[i].Symbol)
         .data(data.Elements[i].DataSeries.close.values)
         .enter()
-        .append('circle')
-      dots.attr('r', 2)
-        .attr('cx', function(d, index) {
-          var date = parseTime.parse(data.Dates[index])
-          return xScale(date)
-        })
-        .attr('cy', function(d, index) {
-          return yScale(d);
-        })
+        .append('g')
+        .attr('class', 'dots data-set')
+        .attr('class', data.Elements[i].Symbol)
+
+      dots.attr('transform', function(d, index) {
+        var date = parseTime.parse(data.Dates[index]);
+        var x = xScale(date) + 50;
+        var y = yScale(d);
+        return 'translate(' + x + ',' + y + ')'
+      })
         .style('fill', $scope.colors[i])
+        .style('stroke', '#222')
+
+      dots.append('circle')
+          .attr('r', 3);
+
+      dots.append('text').text(function(d) {
+          return "$" + d
+        })
+        .style('display', 'none');
+
+      dots.on("mouseenter", function(d, i) {
+        var dot = d3.select(this);
+        dot.select('text')
+           .style('display', 'block');
+      });
+
+      dots.on("mouseleave", function(d, i) {
+        d3.select(this)
+           .select('text')
+           .style('display', 'none');
+      });
+
+
+
       console.log(data)
     }
   };
